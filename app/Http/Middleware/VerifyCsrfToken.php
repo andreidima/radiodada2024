92538c80fd803f6de060a6329bc018012aba64f2
‘Andrei <andrei.dima@usm.ro>
2024-02-07 14:33:29 +0200

    a

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,7 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        '*',
         'https://radiodada.validsoftware.eu/voteaza-si-propune/*',
         'https://radiodada.ro/*',
     ];
