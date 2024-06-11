8ea84743c485c0c3effa7a21c1a5b8279e107ee2
Andrei <andrei.dima@usm.ro>
2024-06-11 15:11:19 +0200

    a

--- app/Http/Controllers/SiteDadaMusicController.php
+++ app/Http/Controllers/SiteDadaMusicController.php
@@ -3,22 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
 
 use App\Models\Artist;
 use App\Models\Imagine;
-use App\Models\Propunere;
+use App\Models\Piesa;
 
 class SiteDadaMusicController extends Controller
 {
     public function afisareArtisti(Artist $artist = null)
     {
+        $artistiDeAfisatInGalerie = Artist::with('imagine')->has('imagine')->select('id', 'nume')->orderBy('nume')->simplePaginate(18);
+
         $artisti = Artist::select('id', 'nume')->orderBy('nume')->get();
 
         // Search this artist in the „propuneri” table, because if any mach is founded, it will be desplayed a link to them in the view
         $propuneri = [];
         if ($artist && $artist->nume){
-            $propuneri = Propunere::select('id', 'nume')
-                ->where('nume', 'like', $artist->nume . '%')
+            $propuneri = Piesa::with('artist')->select('id', 'nume', 'link_youtube')
+                ->whereHas('artist', function (Builder $query) use ($artist) {
+                    $query->where('nume', 'like', $artist->nume);
+                })
+                ->where('categorie', 'like', 'Propunere' . '%')
             ->get();
             // foreach ($propuneri as $propunere){
             //     if (str_contains($propunere->nume, $artist->nume)) {
@@ -28,6 +34,6 @@ class SiteDadaMusicController extends Controller
             // }
         }
 
-        return view('site.dadaMusic.afisareArtisti', compact('artist', 'artisti', 'propuneri'));
+        return view('site.dadaMusic.afisareArtisti', compact('artistiDeAfisatInGalerie', 'artist', 'artisti', 'propuneri'));
     }
 }
--- resources/views/site/dadaMusic/afisareArtisti.blade.php
+++ resources/views/site/dadaMusic/afisareArtisti.blade.php
@@ -7,122 +7,145 @@
 
 @section('content')
 
-<div class="container">
+<div class="container" id="artistAutocomplete">
 
-    <div class="row justify-content-center" id="artistAutocomplete">
-        <div class="col-md-12 p-0">
-            <form class="needs-validation" novalidate method="GET" action="{{ url()->current() }}">
-                @csrf
-                <div class="mb-1 input-group custom-search-form justify-content-center">
-                    {{-- <input type="text" class="form-control" id="search_nume" name="search_nume" placeholder="caută ARTIST" autofocus
-                            value="{{ $search_nume }}" aria-describedby="searchNume"> --}}
-                    {{-- <button class="btn btn-outline-secondary" type="button" id="searchNume">Caută</button> --}}
+    {{-- 1. The page displays the search and the gallery --}}
+    {{-- 2. If there is an artist selected, the page display those artist informations --}}
+    @if (!$artist)
+        <div class="row justify-content-center">
+            <div class="col-md-12 p-0">
+                <div class="row mb-3 rounded-3" style="">
+                    <div class="col-lg-12 mb-2 text-center" style="font-weight:bold">
+                        Descoperă artiștii promovați de postul nostru de radio și piesele lor care s-au regăsit în topurile Dada de-a lungul timpului
+                    </div>
                 </div>
-
-                {{-- <div class="input-group mb-3">
-  <button class="btn btn-outline-secondary" type="button" id="button-addon1">Button</button>
-  <input type="text" class="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1">
-</div> --}}
-
-                {{-- <div class="row input-group custom-search-form justify-content-center">
-                    <button class="btn btn-sm btn-primary col-md-4 me-1 border border-dark rounded-pill" type="submit">
-                        <i class="fas fa-search text-white me-1"></i>Caută
-                    </button>
-                    <a class="btn btn-sm bg-secondary text-white col-md-4 border border-dark rounded-pill" href="{{ url()->current() }}" role="button">
-                        <i class="far fa-trash-alt text-white me-1"></i>Resetează căutarea
-                    </a>
-                </div> --}}
-            </form>
-
-
-        <div class="row mb-4 pt-2 rounded-3" style="">
-            <div class="col-lg-12 mb-2" style="position:relative;" v-click-out="() => artistiListaAutocomplete = ''">
-                <div v-on:focus="autocompleteArtisti();" class="input-group">
-                    <input
-                        type="text"
-                        v-model="artist_nume"
-                        v-on:focus="autocompleteArtisti();"
-                        v-on:keyup="autocompleteArtisti();"
-                        class="form-control bg-white rounded-3 {{ $errors->has('artist_nume') ? 'is-invalid' : '' }}"
-                        name="artist_nume"
-                        placeholder="caută ARTIST"
-                        autocomplete="off"
-                        aria-describedby="artist_nume"
-                        required
-                        style="border:1px black solid"
-                        >
-                </div>
-                <div v-cloak v-if="artistiListaAutocomplete && artistiListaAutocomplete.length" class="panel-footer">
-                    <div class="list-group" style="max-height: 130px; overflow:auto;">
-                        <div v-for="artist in artistiListaAutocomplete">
-                            <a class="list-group-item list-group-item list-group-item-action py-0" :href="'/site/dada-music/artisti/' + artist.id" style="text-decoration: none; color:black">
-                                @{{ artist.nume }}
-                            </a>
+                <div class="row mb-3 rounded-3" style="">
+                    <div class="col-lg-6 mb-2 mx-auto" style="position:relative;" v-click-out="() => artistiListaAutocomplete = ''">
+                        <div v-on:focus="autocompleteArtisti();" class="input-group">
+                            <input
+                                type="text"
+                                v-model="artist_nume"
+                                v-on:focus="autocompleteArtisti();"
+                                v-on:keyup="autocompleteArtisti();"
+                                class="form-control bg-white rounded-3 {{ $errors->has('artist_nume') ? 'is-invalid' : '' }}"
+                                name="artist_nume"
+                                placeholder="search ARTIST"
+                                autocomplete="off"
+                                aria-describedby="artist_nume"
+                                required
+                                style="border:1px black solid"
+                                >
+                        </div>
+                        <div v-cloak v-if="artistiListaAutocomplete && artistiListaAutocomplete.length" class="panel-footer">
+                            <div class="list-group" style="max-height: 130px; overflow:auto;">
+                                <div v-for="artist in artistiListaAutocomplete">
+                                    <a class="list-group-item list-group-item list-group-item-action py-0" :href="'/site/dada-music/artisti/' + artist.id" style="text-decoration: none; color:black">
+                                        @{{ artist.nume }}
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div class="col-md-12 p-0">
+                    <div class="w-100 mb-2 text-center">
+                        @foreach ($artistiDeAfisatInGalerie as $artist)
+                            @if ($artist->imagine)
+                                <a href="{{ env('APP_URL') }}/site/dada-music/artisti/{{ $artist->id }}">
+                                    <img class="rounded-3 m-2"
+                                        src="{{ env('APP_URL') . $artist->imagine->imagine_cale . $artist->imagine->imagine_nume }}"
+                                        title="{{ $artist->nume }}"
+                                        style="
+                                            width: 200px;
+                                            height: 200px;
+                                            object-fit: cover;
+                                        "
+                                        {{-- onerror="this.remove()" --}}
+                                        >
+                                </a>
+                            @endif
+                        @endforeach
+                    </div>
+                    <nav>
+                        <ul class="pagination pagination-sm justify-content-center">
+                            {{$artistiDeAfisatInGalerie->appends(Request::except('page'))->links()}}
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
+    @else
+        <div class="row">
+            <div class="col-lg-12">
+                <a class="mb-4 btn btn-danger rounded-3" href="{{ env('APP_URL') }}/site/dada-music/artisti" style="">
+                    <b>
+                        <i class="fa-solid fa-caret-left"></i>
+                        back
+                    </b>
+                </a>
 
-        @if ($artist)
-            <div class="row">
-                <div class="col-lg-12">
-                    <p class="m-0 mb-3 rounded-3 px-2" style="background-color: gainsboro">{{ $artist->nume }}</p>
+                <h4 class="m-0 mb-3 rounded-3 " style="font-weight:bold">
+                    {{ $artist->nume }}
+                </h4>
 
-                    @if ($artist->imagine)
-                        <div class="w-100 mb-3 text-center">
-                            <a href="{{ env('APP_URL') . $artist->imagine->imagine_cale . $artist->imagine->imagine_nume }}" target="_blank">
-                                <img class="rounded-3" src="{{ env('APP_URL') . $artist->imagine->imagine_cale . $artist->imagine->imagine_nume }}" alt="" style="max-width: 100%">
+                @if ($artist->imagine)
+                    <div class="w-100 mb-3 text-center">
+                        <a href="{{ env('APP_URL') . $artist->imagine->imagine_cale . $artist->imagine->imagine_nume }}" target="_blank">
+                            <img class="rounded-3" src="{{ env('APP_URL') . $artist->imagine->imagine_cale . $artist->imagine->imagine_nume }}" alt="" style="max-width: 100%">
+                        </a>
+                    </div>
+                @endif
+
+                <p class="m-0 mb-3 rounded-3" style="">
+                    {{ $artist->descriere }}
+                </p>
+
+                <div v-if="afiseazaPropuneri" class="m-0 mb-4 rounded-3 px-3 text-center" style="">
+                    <p class="m-0 rounded-3 py-0 px-2 text-center" style="">
+                        PROPUNERI
+                    </p>
+                    @foreach ($propuneri as $propunere)
+                        <p class="m-0 mb-3 rounded-3 px-2" style="">
+                                @if ($propunere->link_youtube)
+                                    <a href="{{ $propunere->link_youtube }}" target="_blank" style="text-decoration:none">
+                                        {{ $propunere->nume }}
+                                    </a>
+                                @else
+                                    {{ $propunere->nume }}
+                                @endif
+                        </p>
+                    @endforeach
+                </div>
+
+                <div class="d-flex justify-content-center">
+                    @if ($artist->link)
+                        <div class="mx-3">
+                            <a href="{{ $artist->link }}" target="_blank" style="color: black">
+                                <i class="fa-regular fa-user fa-3x"></i>
+                            </a>
+                        </div>
+                    @endif
+                    @if ($artist->magazin_virtual)
+                        <div class="mx-3">
+                            <a href="{{ $artist->magazin_virtual }}" style="color: black">
+                                <i class="fa-solid fa-cart-shopping fa-3x"></i>
                             </a>
                         </div>
                     @endif
 
-                    <p class="m-0 mb-3 rounded-3 px-2" style="background-color: gainsboro">
-                        {{ $artist->descriere }}
-                    </p>
-
-                    <div v-if="afiseazaPropuneri" class="m-0 mb-3 rounded-3 px-2 text-center" style="background-color: gainsboro">
-                        <p class="m-0 rounded-3 px-2 text-center" style="background-color: gainsboro">
-                            PROPUNERI
-                        </p>
-                        @foreach ($propuneri as $propunere)
-                            <p class="m-0 mb-3 rounded-3 px-2" style="background-color: gainsboro">
-                                {{ $propunere->nume }}
-                            </p>
-                        @endforeach
-                    </div>
-
-
-                    <div class="d-flex justify-content-center">
-                        @if ($artist->link)
-                            <div class="mx-3">
-                                <a href="{{ $artist->link }}" target="_blank" style="color: black">
-                                    <i class="fa-regular fa-user fa-3x"></i>
-                                </a>
-                            </div>
-                        @endif
-                        @if ($artist->magazin_virtual)
-                            <div class="mx-3">
-                                <a href="{{ $artist->magazin_virtual }}" style="color: black">
-                                    <i class="fa-solid fa-cart-shopping fa-3x"></i>
-                                </a>
-                            </div>
-                        @endif
-
-                        @if ($propuneri->count() > 0)
-                            <div class="mx-3">
-                                <a
-                                    v-on:click="afiseazaPropuneri = !afiseazaPropuneri"
-                                style="color: black">
-                                    <i class="fa-solid fa-headphones fa-3x"></i>
-                                </a>
-                            </div>
-                        @endif
-                    </div>
+                    @if ($propuneri->count() > 0)
+                        <div class="mx-3">
+                            <a
+                                v-on:click="afiseazaPropuneri = !afiseazaPropuneri"
+                            style="color: black">
+                                <i class="fa-solid fa-headphones fa-3x"></i>
+                            </a>
+                        </div>
+                    @endif
                 </div>
             </div>
-        @endif
-    </div>
+        </div>
+    @endif
 
 
 </div>
